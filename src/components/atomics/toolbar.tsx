e1311bd5b0add5b4c8aa7ca0fc9a89c455e285bc
afeiship <1290657123@qq.com>
2021-01-25 15:39:46 +0800

    docs: update toolbar

--- src/components/atomics/toolbar.tsx
+++ src/components/atomics/toolbar.tsx
@@ -7,8 +7,6 @@ const CLASS_NAME = 'react-rte-slate__toolbar';
 export default class extends React.Component<Props> {
   public render() {
     const { children } = this.props;
-    return <nav className={CLASS_NAME}>
-      {children}
-    </nav>
+    return <nav className={CLASS_NAME}>{children}</nav>;
   }
 }
