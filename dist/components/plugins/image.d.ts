47a63f633ea3bc21ef5e1f728e8d34978691bd9a
afeiship <1290657123@qq.com>
2021-01-31 12:31:49 +0800

    fix: missing commit

---
+++ dist/components/plugins/image.d.ts
@@ -0,0 +1,21 @@
+/// <reference types="react" />
+import { Element } from 'slate';
+declare const _default: {
+    name: string;
+    hooks: {
+        element: (inContext: any, inProps: any) => JSX.Element | undefined;
+    };
+    decorator: (editor: any) => any;
+    importer: (el: any, children: any) => Element | undefined;
+    exporter: (node: any, children: any) => string | undefined;
+};
+export default _default;
+/**
+
+const element = {
+  type: 'image',
+  url: 'https://himg.bdimg.com/sys/portrait/item/be10475f686d6c73db00.jpg',
+  children: [{ text: '' }]
+};
+Transforms.insertNodes(this.editor, element);
+ */
---
+++ dist/components/types.d.ts
@@ -0,0 +1,26 @@
+/// <reference types="react" />
+import { Editor } from 'slate';
+export interface Entity {
+    name: string;
+    disabled?: boolean;
+    decorator?: (editor: Editor) => Editor;
+    importer?: any;
+    exporter?: any;
+    hooks?: {
+        leaf: (context: any, editor: Editor) => JSX.Element | null;
+        element: (context: any, editor: Editor) => JSX.Element | null;
+    };
+}
+export interface EventTarget {
+    target: {
+        value: any;
+    };
+}
+export declare type Props = {
+    className?: string;
+    value?: string;
+    placeholder?: string;
+    onInit?: (event: EventTarget) => void;
+    onChange?: (event: EventTarget) => void;
+    plugins: Array<Entity>;
+};
