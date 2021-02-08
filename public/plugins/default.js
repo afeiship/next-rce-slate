2b0065384cea0ed64354bdaa93e1b8ad7140b66a
afeiship <1290657123@qq.com>
2021-02-08 15:12:23 +0800

    fix: defalut element

---
+++ public/plugins/default.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { jsx } from 'slate-hyperscript';
+import NxSlatePlugin from '@jswork/next-slate-plugin';
+import NxCssText from '@jswork/next-css-text';
+import nxIsBlockElement from '@jswork/next-is-block-element';
+/**
+ * @usage:
+ * Transforms.setNodes(editor, { type:'default' })
+ *
+ * 注意这里的 style处理，所有的 block 元素，都有可能被直接设置 style;
+ * input/output 都需要针对style写一些代码
+ */
+
+export default NxSlatePlugin.define({
+  id: 'default',
+  serialize: {
+    input: ({ el }, children) => {
+      if (nxIsBlockElement(el)) {
+        const css = el.style.cssText;
+        return jsx('element', { type: 'default', style: NxCssText.css2obj(css) }, children);
+      }
+    },
+    output: (node, children) => {
+      const { style } = node;
+      return `<p${style}>${children}</p>`;
+    }
+  },
+  render: (_, { attributes, children, element }) => {
+    return <p {...attributes}>{children}</p>;
+  }
+});
